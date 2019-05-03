250354ed0f6bac0837eacdc7b7b5cdaadbe900cd
beautyyuyanli <beautyyuyanli@outlook.com>
2019-05-03 21:45:38 +0800

    /games

--- games/AlarmQAQ/main.js
+++
@@ -1,6 +0,0 @@
-var c_bottom = 0,c_opacity = 1,num = 60;
-function alarm_(){
-    var c = document.getElementById("continue_1s");
-    c.style.bottom = (c_bottom += 10) + "px";
-    c.style.opacity = (c_opacity -= 0.04);
-}
