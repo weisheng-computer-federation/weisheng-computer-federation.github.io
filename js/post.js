ece9dda5e57d99bab8a5ea4144029c405edf3425
beautyyuyanli <beautyyuyanli@outlook.com>
2019-07-15 23:27:29 +0800

    添加页面 历届管理团队 & 修复移动设备易用性问题

--- js/post.js
+++ js/post.js
@@ -3,4 +3,5 @@ function initialization(){
     for (let i = 0;i < imgs.length;++ i){
         $(imgs[i]).parent().css("text-align", "center");
     }
+    $(".content.post img").css("max-width", Math.min($('.post').innerWidth() - 20, 700) + 'px');
 }
