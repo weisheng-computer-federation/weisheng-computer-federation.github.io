e37907397bc89cb1ecb4c390dd70a0f524a90351
luawig <2453576227@qq.com>
2019-07-16 12:52:31 +0800

    updata

--- posts/convert.py
+++ posts/convert.py
@@ -1,14 +1,14 @@
 import markdown2
 import os
-for dirName, subdirList, fileList in os.walk('./html/'):
+for dirName, subdirList, fileList in os.walk('posts/html/'):
     for fname in fileList:
         os.remove(dirName + fname)
-tem = open('./post.template.html', 'r').read()
-for dirName, subdirList, fileList in os.walk('./md/'):
+tem = open('posts/post.template.html', 'r', encoding='utf-8').read()
+for dirName, subdirList, fileList in os.walk('posts/md/'):
     for fname in fileList:
         mdcont = str(markdown2.markdown_path(dirName + fname))
         htmlcont = tem
         htmlcont = htmlcont.replace('$-=contents=-$', mdcont)
         htmlcont = htmlcont.replace('$-=title=-$', fname.split('.')[0])
-        htmlfile = open('./html/' + fname.split('.')[0] + '.html', 'w')
+        htmlfile = open('posts/html/' + fname.split('.')[0] + '.html', 'w', encoding='utf-8')
         htmlfile.write(htmlcont)
