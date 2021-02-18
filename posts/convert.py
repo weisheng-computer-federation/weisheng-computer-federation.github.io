87f2a277f41c57a2cbb33e0c0e8b3dadbf0f4c1c
Zerbats <1348329118@qq.com>
2021-02-18 20:38:21 +0800

    更新了第四届1699的相关信息

--- posts/convert.py
+++ posts/convert.py
@@ -1,14 +1,14 @@
 import markdown2
 import os
-for dirName, subdirList, fileList in os.walk('posts/html/'):
+for dirName, subdirList, fileList in os.walk('html/'):
     for fname in fileList:
         os.remove(dirName + fname)
-tem = open('posts/post.template.html', 'r', encoding='utf-8').read()
-for dirName, subdirList, fileList in os.walk('posts/md/'):
+tem = open('post.template.html', 'r', encoding='utf-8').read()
+for dirName, subdirList, fileList in os.walk('md/'):
     for fname in fileList:
         mdcont = str(markdown2.markdown_path(dirName + fname))
         htmlcont = tem
         htmlcont = htmlcont.replace('$-=contents=-$', mdcont)
         htmlcont = htmlcont.replace('$-=title=-$', fname.split('.')[0])
-        htmlfile = open('posts/html/' + fname.split('.')[0] + '.html', 'w', encoding='utf-8')
+        htmlfile = open('html/' + fname.split('.')[0] + '.html', 'w', encoding='utf-8')
         htmlfile.write(htmlcont)
